4e0cb6239c828e1c1c2ae79533c4e80c2bbc1aa3
= <=>
2025-03-09 11:52:25 +0500

    show-student-with-delete

--- src/app/show-student/show-student.component.ts
+++ src/app/show-student/show-student.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-show-student',
@@ -6,6 +7,48 @@ import { Component } from '@angular/core';
   templateUrl: './show-student.component.html',
   styleUrl: './show-student.component.css'
 })
-export class ShowStudentComponent {
+
+export class ShowStudentComponent implements OnInit {
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getAllStudentData();
+  }
+
+  students: any[] = [];
+      getAllStudentData() {
+        // Fetch data from your server here using HttpClient
+        const url = "http://localhost:8080/students"
+
+        this.httpClient.get(url).subscribe((response: any) => {
+          console.log(response);
+          this.students = response;
+        }, (error) => {
+          console.error("Error in Fetching Record", error);
+        });
+
+      }
+
+// deleting student record on the basis of id 
+
+deleteStudent(studentId: any) {
+
+  const url = "http://localhost:8080/student/delete"
+
+  this.httpClient.get(url + "/" + studentId).subscribe((response: any) => {
+    console.log(response);
+    this.getAllStudentData();
+    
+  }, (error) => {
+    console.error("Error in deleting Record", error);
+  });
+
+}
+
+editStudent(student:any){
+  console.log(student);
+}
+
 
 }
